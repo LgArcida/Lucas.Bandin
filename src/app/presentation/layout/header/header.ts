890f260ba176a2ef1caf389e3c2c7b6b07e02a77
lucas-bandin <lucas.bandin.arcida@gmail.com>
2026-07-26 23:02:09 -0400

    Presentation folder

---
+++ src/app/presentation/layout/header/header.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageSwitcherComponent } from '../../shared/language-switcher/language-switcher';
+import { NAV_ITEMS, NavItem } from '@application/navigation/nav-items';
+import { NavigationService } from '@application/navigation/navigation.service';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatButtonModule,
+    MatToolbarModule,
+    TranslatePipe,
+    LanguageSwitcherComponent,
+  ],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+  readonly menuToggle = output<void>();
+  readonly #navigation = inject(NavigationService);
+  protected readonly navItems = NAV_ITEMS;
+
+  protected navigate(item: NavItem): void {
+    if (item.targetId) {
+      this.#navigation.scrollTo(item.targetId);
+    }
+  }
+}
