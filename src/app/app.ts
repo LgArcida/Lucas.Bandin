64f70a135f1e1d92a1bda82d616fb2536e2ea1a8
lucas-bandin <lucas.bandin.arcida@gmail.com>
2026-08-12 09:13:17 -0400

    Full DDD structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,16 @@
 import { ChangeDetectionStrategy, Component, DOCUMENT, effect, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { AppStore } from '@application/state/app.store';
-import { HeaderComponent } from './presentation/layout/header/header';
-import { SidenavMenuComponent } from './presentation/layout/sidenav-menu/sidenav-menu';
-import { AboutComponent } from './presentation/features/about-me/about-me';
-import { MeComponent } from './presentation/features/me/me';
-import { SkillsComponent } from './presentation/features/skills/skills';
-import { FooterComponent } from './presentation/layout/footer/footer';
-import { ExperienceComponent } from './presentation/features/experience/experience';
-import { ProjectsComponent } from './presentation/features/projects/projects';
-import { EducationComponent } from './presentation/features/education/education';
-import { BeyondCodeComponent } from './presentation/features/beyond-code/beyond-code';
+import { HeaderComponent } from '@presentation/layout/header/header';
+import { SidenavMenuComponent } from '@presentation/layout/sidenav-menu/sidenav-menu';
+import { AboutComponent } from '@presentation/features/about-me/about-me';
+import { MeComponent } from '@presentation/features/me/me';
+import { SkillsComponent } from '@presentation/features/skills/skills';
+import { FooterComponent } from '@presentation/layout/footer/footer';
+import { ExperienceComponent } from '@presentation/features/experience/experience';
+import { ProjectsComponent } from '@presentation/features/projects/projects';
+import { EducationComponent } from '@presentation/features/education/education';
+import { BeyondCodeComponent } from '@presentation/features/beyond-code/beyond-code';
 
 @Component({
   selector: 'app-root',
